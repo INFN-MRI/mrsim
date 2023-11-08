5de4bf3bd7ee06a2e553ad848dc501788ee188c0
mcencini <matteo.cencini@gmail.com>
2023-11-08 11:56:48 +0100

    Better looking derivative figure

--- examples/scripts/02-derivatives.py
+++ examples/scripts/02-derivatives.py
@@ -77,7 +77,7 @@ def crlb_finitediff_cost(flip, ESP, T1, T2):
 t1 = 1000.0
 t2 = 100.0
 
-angles = np.ones(60, dtype=np.float32) * 120.0
+angles = np.concatenate((np.linspace(0, 180.0, 36), np.ones(60, dtype=np.float32) * 180.0))
 esp = 5.0
 
 # run
@@ -104,21 +104,21 @@ t1 = time.time()
 tcost = t1 - t0
 
 # plot derivative
-#%%
+#%%plots 
 fsz = 20
 plt.figure()
 plt.subplot(2,2,1)
 plt.rcParams.update({'font.size': 0.5 * fsz})
 plt.plot(angles, '.')
 plt.xlabel("Echo #", fontsize=fsz)
-plt.xlim([-1, 51])
+plt.xlim([-1, 97])
 plt.ylabel("Flip Angle [deg]", fontsize=fsz)
 
 plt.subplot(2,2,2)
 plt.rcParams.update({'font.size': 0.5 * fsz})
 plt.plot(abs(grad), '-k'), plt.plot(abs(grad0), '*r')
 plt.xlabel("Echo #", fontsize=fsz)
-plt.xlim([-1, 51])
+plt.xlim([-1, 97])
 plt.ylabel(r"$\frac{\partial signal}{\partial T2}$ [a.u.]", fontsize=fsz)
 plt.legend(["Finite Diff", "Auto Diff"])
 
@@ -127,7 +127,7 @@ plt.subplot(2,2,3)
 plt.rcParams.update({'font.size': 0.5 * fsz})
 plt.plot(abs(dcost), '-k'), plt.plot(abs(dcost0), '*r')
 plt.xlabel("Echo #", fontsize=fsz)
-plt.xlim([-1, 51])
+plt.xlim([-1, 97])
 plt.ylabel(r"$\frac{\partial CRLB}{\partial FA}$ [a.u.]", fontsize=fsz)
 plt.legend(["Finite Diff", "Auto Diff"])
 
@@ -148,7 +148,7 @@ rects2 = plt.bar(x - width/2, time_auto, width, label='Auto Diff')
 # Add some text for labels, title and custom x-axis tick labels, etc.
 plt.ylabel('Execution Time [s]', fontsize=fsz)
 plt.xticks(x, labels, fontsize=fsz)
-plt.ylim([0, 25])
+# plt.ylim([0, 25])
 plt.legend()
 
 plt.bar_label(rects1, padding=3, fontsize=fsz)
